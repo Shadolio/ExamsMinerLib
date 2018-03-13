bd7d7d6221f3680ac0eb0014da1c68f69acc0f71
Shadi Barghash <shadolioware@gmail.com>
2018-03-13 20:25:47 +0200

    Add code documentation for Course struct
    Signed-off-by: Shadi Barghash <shadolioware@gmail.com>

--- .NET Universal Windows 8.1/ExamsMinerLib/ExamsMinerLib/Model/Course.cs
+++ .NET Universal Windows 8.1/ExamsMinerLib/ExamsMinerLib/Model/Course.cs
@@ -1,5 +1,4 @@
-﻿/**
- * Copyright 2018 Shadi Barghash
+﻿/** Copyright 2018 Shadi Barghash
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +15,35 @@
 
 namespace ExamsMinerLib.Model
 {
+    /// <summary>
+    /// Represents a subject course that an exam is typically linked to.
+    /// </summary>
+    /// <typeparam name="TLevel">Specifies the structure that is used to select the level of the course. Can be a primitive, struct, class or interface.</typeparam>
     public struct Course<TLevel>
     {
 
+        /// <summary>
+        /// Gets the Course Code. Usually the one used for identification, e.g. "CSEN202" (for Degree courses) or "9602" (for IGCSE).
+        /// </summary>
         public string Code { get; private set; }
 
+        /// <summary>
+        /// Gets the course's subject. Usually more friendly descriptor than <see cref="Code"/>, e.g. "Accounting".
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Show the level of this Course. The way levels are modelled may differ from one enviroment to another.
+        /// </summary>
+        /// <remarks>
+        /// Helpful when a subject has courses of different levels, e.g. Accounting OL vs. AS vs. AL,
+        /// or to just describe it for knowledge, that might be used for search later.
+        /// </remarks>
         public TLevel Level { get; private set; }
 
+        /// <summary>
+        /// Describe a new Course that can be used to describe an Exam.
+        /// </summary>
         public Course(string code, string name, TLevel level)
         {
             Code = code;
